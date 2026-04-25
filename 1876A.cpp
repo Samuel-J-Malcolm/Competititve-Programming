a543a6456ec4976586a906fc6aaec479c2d32dde
Samuel <samuel.malcolm.cs@gmail.com>
2026-04-25 06:04:57 -0500

    neww changes

--- 1876A.cpp
+++ 1876A.cpp
@@ -18,6 +18,8 @@ using vi  = vector<int>;
 using vll = vector<ll>;
 using vvi = vector<vi>;
 using vvll = vector<vll>;
+using vpii = vector<pii>;
+
 
 // Macros
 #define pb    push_back
@@ -29,6 +31,8 @@ using vvll = vector<vll>;
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x)   (int)(x).size()
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rep(i, a) for (int i = (0); i < (a); i++)
+
 
 // Constants
 const ll INF  = 1e18;
@@ -38,7 +42,35 @@ const ll  MOD = 1e9 + 7;
 
 void solve()
 {
-    
+    vpii list;
+    int c1;
+    int n;
+    cin >> n;
+    cin >> c1;
+    rep(i,n){
+        int a;
+        cin >> a;
+        list.push_back({0,a});
+    }
+    rep(i,n){
+        int a;
+        cin >> a;
+        list[i].fi = a;
+    }
+    list.push_back({c1,INF32});
+    sort(all(list));
+    ll cost = c1;
+    n--;
+    ll min1;
+    for(pii i: list){
+        min1 = min(i.se,n);
+        n -= min1;
+        cost += min1* ((ll)i.fi); 
+        if(n == 0){
+            break;
+        }
+    }
+    cout << cost << "\n";
 }
 
 int main()
---
+++ 1883C.cpp
@@ -0,0 +1,112 @@
+/*
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pil = pair<int, ll>;
+
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+
+// Macros
+#define pb    push_back
+#define eb    emplace_back
+#define mp    make_pair
+#define fi    first
+#define se    second
+#define all(x)  (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x)   (int)(x).size()
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+
+// Constants
+const ll INF  = 1e18;
+const int INF32 = 1e9;
+const ld  EPS = 1e-9;
+const ll  MOD = 1e9 + 7;
+
+void solve()
+{
+    int n = 1;
+    cin >> n;
+    int k=1;
+    cin >> k;
+    int max = 4;
+    int x = 0;
+    if(k == 2){
+        rep(i,0,n){
+            cin >> x;
+            max = min(max,x % 2);
+        }
+    }
+    if(k == 3){
+        rep(i,0,n){
+            cin >> x;
+            max = min(max,3 - (x % 3));
+            if((x % 3) == 0){
+                max = 0;
+            }
+        }
+    }
+    if(k == 5){
+        rep(i,0,n){
+            cin >> x;
+            max = min(max,5 - (x % 5));
+            if((x % 5) == 0){
+                max = 0;
+            }
+        }
+    }
+    if(k == 4){
+        int num = 0;
+        rep(i,0,n){
+            cin >> x;
+            max = min(max,4 - (x % 4));
+            if(x % 4 == 2){
+                num++;
+                if(num > 1){
+                    max = 0;
+                }
+                
+            }
+            if((x % 4) == 0){
+                max = 0;
+            }
+        }
+        if(max == 3 && n > 1){
+            max = 2;
+        }
+        if(max == 2 && num >= 1 && n >= 2){
+            max = 1;
+        }
+    }
+    cout << max << "\n";
+
+
+    
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
